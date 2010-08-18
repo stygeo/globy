55d9381114dc6f2889d9972fa3a511f763367c54
Jeffrey Wilcke <jeffrey@noxa.nl>
2010-08-18 22:08:39 +0200

    Init

--- app/models/user_subscription.rb
+++ app/models/user_subscription.rb
@@ -1,4 +1,4 @@
 class UserSubscription < ActiveRecord::Base
-	belongs_to :user
-	belongs_to :project
+	belongs_to :subscriber, :foreign_key => :user_id
+	belongs_to :subscription, :source => :project, :foreign_key => :project_id
 end
